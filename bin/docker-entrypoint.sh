de1e0cc0ccd3608508f17c9f5a5cb2d9348c820f
Albatroz <118113700+ARUNKUMAAR-R@users.noreply.github.com>
2024-04-18 12:02:29 +0530

    Update docker-entrypoint.sh

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # If running the rails server then create or migrate existing database
 if [ "${*}" == "./bin/rails server" ]; then
-  ./bin/rails db:create && ./bin/rails db:migrate RAILS_ENV=development
+  ./bin/rails db:create && bin/rails db:migrate RAILS_ENV=development
   
 fi
 
