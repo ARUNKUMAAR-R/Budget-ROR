00fed5893b938e8d575c621243211b3760b4f81c
arun <arun@JAMMY.myguest.virtualbox.org>
2024-04-18 11:23:05 +0530

    entrypoint script updated

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # If running the rails server then create or migrate existing database
-if [ "${*}" == "./bin/rails server" ]; then
-  ./bin/rails db:create
-  
+if [ "${*}" == "./bin/rails server -b 0.0.0.0 -p 3000" ]; then
+  ./bin/rails db:create &&
+  ./bin/rails db:migrate  
 fi
 
 exec "${@}"
